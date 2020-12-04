e49c7e72fddac481c0850d615404b7b8fdcf8319
Alex Kolesnik <alexeykolesnik7@gmail.com>
2020-12-05 01:15:34 +0200

    wrong link error fix

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -9,8 +9,8 @@ const Letter = () => {
   const [visibility, setVisibility] = useState(false);
   let { name } = useParams();
   let guestObj = guestList.find((obj) => obj.name === name);
-  let splitString = guestObj.female ? 'разделила' : 'разделил';
   if (!!guestObj) {
+    let splitString = guestObj.female ? "разделила" : "разделил";
     return (
       <div className="first-page">
         <div className="letter-wrapper">
@@ -22,19 +22,27 @@ const Letter = () => {
             <h2 className="letter-text">{guestObj.greeting}</h2>
             <Typist.Delay ms={1000} />
             <p className="letter-text small-text">
-              {guestObj.plural ? `Мы хотим чтобы вы разделили ` : `Мы хотим чтобы ты ${splitString} `}
-              с нами один из самых важных дней в нашей жизни и поэтому
-              счастливы пригласить {guestObj.plural ? "вас" : "тебя"} на христианскую речь в честь нашего
-              бракосочетания. Она состоится 12-го декабря в 14:00.
+              {guestObj.plural
+                ? `Мы хотим чтобы вы разделили `
+                : `Мы хотим чтобы ты ${splitString} `}
+              с нами один из самых важных дней в нашей жизни и поэтому счастливы
+              пригласить {guestObj.plural ? "вас" : "тебя"} на христианскую речь
+              в честь нашего бракосочетания. Она состоится 12-го декабря в
+              14:00.
             </p>
-            <p className="letter-text small-text text-right">Ваши Алексей и Лолита:)</p>
+            <p className="letter-text small-text text-right">
+              Ваши Алексей и Лолита:)
+            </p>
           </Typist>
-          <hr/>
+          <hr />
           <div className={`hidden ${visibility ? "showed" : ""}`}>
-            <p className="letter-text small-text countdown-text">Ссылка на конференцию появится ниже через:</p>
+            <p className="letter-text small-text countdown-text">
+              Ссылка на конференцию появится ниже через:
+            </p>
             <WeddingCoundown />
             <p className="letter-text tooltip">
-              Больше информации {guestObj.plural ? "вы найдете " : "ты найдешь "}
+              Больше информации{" "}
+              {guestObj.plural ? "вы найдете " : "ты найдешь "}
               <Link className="info-link" to="/info">
                 здесь
               </Link>
