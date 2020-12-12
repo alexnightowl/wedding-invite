d30af7a945d7cf2fbdba279307d8bc0dae2a2f23
Alex Kolesnik <alexeykolesnik7@gmail.com>
2020-12-12 12:30:06 +0200

    link update

--- src/components/ZoomButton.jsx
+++ src/components/ZoomButton.jsx
@@ -4,9 +4,9 @@ const ZoomButton = () => {
     return (
         <div className="zoom-link-wrap">
             <h3 className="letter-text zoom-link-header">Добро пожаловать!</h3>
-            <a className="zoom-link" href="google.com">Ссылка на конференцию</a>
-            <p>Идентификатор: 123456789</p>
-            <p>Пароль: sdfda532</p>
+            <a className="zoom-link" href="https://jworg.zoom.us/j/4123917376">Ссылка на конференцию</a>
+            <p>Идентификатор: 412 391 7376</p>
+            <p>Пароль: 121220</p>
         </div>
     )
 }
